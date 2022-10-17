6ed4522017b9c26f04505c678dff8ad0749d2861
Matheus Macedo <matheusg3.201168@gmail.com>
2022-10-17 19:03:25 -0300

    adicionado o texto sobre mim e minhas redes

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,27 @@ const abas = document.querySelectorAll(".aba");
 
 abas.forEach((aba) => {
   aba.addEventListener("click", () => {
+    if (aba.classList.contains('selecionado')) {
+      return;
+    }
+
     const abaSelecionada = document.querySelector(".aba.selecionado");
     abaSelecionada.classList.remove("selecionado");
 
     aba.classList.add("selecionado");
 
-    const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+    const informacaoSelecionada = document.querySelector(
+      ".informacao.selecionado"
+    );
 
-    informacaoSelecionada.classList.remove('selecionado')
+    informacaoSelecionada.classList.remove("selecionado");
 
-    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`;
 
-    const informacaoParaMostrar = document.getElementById(idDoElementoDeInformacoesDaAba)
+    const informacaoParaMostrar = document.getElementById(
+      idDoElementoDeInformacoesDaAba
+    );
 
-    informacaoParaMostrar.classList.add('selecionado')
-
+    informacaoParaMostrar.classList.add("selecionado");
   });
 });
