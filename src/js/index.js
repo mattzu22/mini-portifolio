3ac8db6313b59b18c1952e6d34a6f81f72d8f63c
Matheus Macedo <matheusg3.201168@gmail.com>
2022-10-16 23:57:47 -0300

    commit inicial

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach((aba) => {
+  aba.addEventListener("click", () => {
+    const abaSelecionada = document.querySelector(".aba.selecionado");
+    abaSelecionada.classList.remove("selecionado");
+
+    aba.classList.add("selecionado");
+
+    const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+
+    informacaoSelecionada.classList.remove('selecionado')
+
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+
+    const informacaoParaMostrar = document.getElementById(idDoElementoDeInformacoesDaAba)
+
+    informacaoParaMostrar.classList.add('selecionado')
+
+  });
+});
